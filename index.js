b947e4beac49163bbf4375746867a7afc2233b2b
Neptune <neptunecurrency@gmail.com>
2023-02-04 13:09:15 +0530

    port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT;
 const webhookClient = new Discord.WebhookClient({ 
     url: process.env.WEBHOOK,
 });
